2b3b5bef9468dcba2e39e74aafa62da622e7de71
KleberNeves <kleber.na@gmail.com>
2021-06-16 15:39:32 -0300

    Fix package dependencies

--- R/crm.R
+++ R/crm.R
@@ -21,6 +21,7 @@
 #' @param save_files If NULL (default), the data will not be saved to disk. If you provide a path, it will save the results to the given folder.
 #'
 #' @return A list containing two data frames, one with the references (citing and cited) and one with the data underlying the RPYS graph
+#' @importFrom magrittr %>%
 #' @export
 run_CRE <- function (
   data_files = NULL,
@@ -61,19 +62,21 @@ run_CRE <- function (
 
   # prepare CRE script and run it
   cre_script = CRM_base_CRE_jar_script_string %>%
-    str_replace("_FILE_LIST_", files_list_str) %>%
-    str_replace("_TYPE_", type) %>%
-    str_replace("_MIN_RPY_", as.character(cited_year_range[1])) %>%
-    str_replace("_MAX_RPY_", as.character(cited_year_range[2])) %>%
-    str_replace("_NO_RPY_", ifelse(include_cited_wo_year, "true", "false")) %>%
-    str_replace("_MIN_PY_", as.character(citing_year_range[1])) %>%
-    str_replace("_MAX_PY_", as.character(citing_year_range[2])) %>%
-    str_replace("_NO_PY_", ifelse(include_citing_wo_year, "true", "false")) %>%
-    str_replace("_SAMPLING_", sampling) %>%
-    str_replace("_OFFSET_", as.character(offset)) %>%
-    str_replace("_MAXCR_", as.character(import_max)) %>%
-    str_replace("_EXPORT_GRAPH_", fn_graph) %>%
-    str_replace("_EXPORT_CITED_", fn_cited)
+    stringr::str_replace("_FILE_LIST_", files_list_str) %>%
+    stringr::str_replace("_TYPE_", type) %>%
+    stringr::str_replace("_MIN_RPY_", as.character(cited_year_range[1])) %>%
+    stringr::str_replace("_MAX_RPY_", as.character(cited_year_range[2])) %>%
+    stringr::str_replace("_NO_RPY_",
+                         ifelse(include_cited_wo_year, "true", "false")) %>%
+    stringr::str_replace("_MIN_PY_", as.character(citing_year_range[1])) %>%
+    stringr::str_replace("_MAX_PY_", as.character(citing_year_range[2])) %>%
+    stringr::str_replace("_NO_PY_",
+                         ifelse(include_citing_wo_year, "true", "false")) %>%
+    stringr::str_replace("_SAMPLING_", sampling) %>%
+    stringr::str_replace("_OFFSET_", as.character(offset)) %>%
+    stringr::str_replace("_MAXCR_", as.character(import_max)) %>%
+    stringr::str_replace("_EXPORT_GRAPH_", fn_graph) %>%
+    stringr::str_replace("_EXPORT_CITED_", fn_cited)
 
   crs_file = file.path(tempdir(), "script.crs")
   readr::write_lines(cre_script, path = crs_file)
@@ -83,22 +86,22 @@ run_CRE <- function (
   system(jar_call)
 
   # read files
-  data_graph = read_csv(fn_graph, col_types = cols()) %>%
-    select(-AVG) %>%
+  data_graph = readr::read_csv(fn_graph, col_types = readr::cols()) %>%
+    dplyr::select(-AVG) %>%
     `colnames<-`(c("Year", "# Cited References", "Deviation from 5-year median"))
 
   data_cited = suppressWarnings(suppressMessages(
-    read_csv(fn_cited, col_types = cols(), na = c("", "null"))
+    readr::read_csv(fn_cited, col_types = readr::cols(), na = c("", "null"))
   )) %>%
-    select(ID, TI, PY, SO, AU, DI, ID_1, CR, RPY, N_CR, DOI) %>%
+    dplyr::select(ID, TI, PY, SO, AU, DI, ID_1, CR, RPY, N_CR, DOI) %>%
     `colnames<-`(c("ID_Citing", "Title_Citing", "Year_Citing", "Journal_Citing",
                    "Author_Citing", "DOI_Citingr", "ID_Cited", "Reference_Cited",
                    "Year_Cited", "Times_Cited", "DOI_Cited"))
 
   # save files if desired
   if (!is.null(save_files)) {
-    write_tsv(data_graph, file.path(save_files, "CRE_data_graph.tsv"))
-    write_tsv(data_cited, file.path(save_files, "CRE_cited_citing_refs.tsv"))
+    readr::write_tsv(data_graph, file.path(save_files, "CRE_data_graph.tsv"))
+    readr::write_tsv(data_cited, file.path(save_files, "CRE_cited_citing_refs.tsv"))
   }
 
   # return data frames
@@ -122,6 +125,7 @@ run_CRE <- function (
 #' @param cited_year_range Passed to the plotting function (the analysis will take up the full range)
 #'
 #' @return A list containing a data frame with the milestone papers, a vector of the milestone years and two options of RPYS figures (ggplot objects)
+#' @importFrom magrittr %>%
 #' @export
 identify_milestones <- function (CR_data, n_milestones = 10, n_refs = 1, min_times_cited = 2, cited_year_range = NULL) {
 
@@ -133,41 +137,43 @@ identify_milestones <- function (CR_data, n_milestones = 10, n_refs = 1, min_tim
 
   # Find peak years
   df = CR_data$references %>%
-    filter(Times_Cited >= min_times_cited) %>%
-    count(Year_Citing, Year_Cited) %>%
-    group_by(Year_Citing) %>%
-    mutate(
+    dplyr::filter(Times_Cited >= min_times_cited) %>%
+    dplyr::count(Year_Citing, Year_Cited) %>%
+    dplyr::group_by(Year_Citing) %>%
+    dplyr::mutate(
       dt = detrend(n),
       r = rank(dt, ties.method = "min"),
       r = 100 + r - max(r)
     )
 
-  dfs = df %>% group_by(Year_Cited) %>%
-    summarise(M = mean(r), SE = sd(r) / n(), D = M / SE)
+  dfs = df %>% dplyr::group_by(Year_Cited) %>%
+    dplyr::summarise(M = mean(r, na.rm = T))
 
-  peaks = dfs %>% slice_max(order_by = M, n = n_milestones) %>% pull(Year_Cited)
+  peaks = dfs %>%
+    dplyr::slice_max(order_by = M, n = n_milestones) %>%
+    dplyr::pull(Year_Cited)
 
-  df = df %>% mutate(Peak = Year_Cited %in% peaks)
+  df = df %>% dplyr::mutate(Peak = Year_Cited %in% peaks)
 
   p1 = plot_multi_rpys(CR_data, cited_year_range = cited_year_range) +
-    geom_vline(xintercept = peaks - 0.5, size = 1, alpha = 0.4, color = "red") +
-    geom_vline(xintercept = peaks + 0.5, size = 1, alpha = 0.4, color = "red") +
-    theme(legend.position = "none")
+    ggplot2::geom_vline(xintercept = peaks - 0.5, size = 1, alpha = 0.4, color = "red") +
+    ggplot2::geom_vline(xintercept = peaks + 0.5, size = 1, alpha = 0.4, color = "red") +
+    ggplot2::theme(legend.position = "none")
 
   p2 = plot_rpys(CR_data, cited_year_range = cited_year_range) +
-    geom_vline(xintercept = peaks - 0.5, size = 1, alpha = 0.4, color = "red") +
-    geom_vline(xintercept = peaks + 0.5, size = 1, alpha = 0.4, color = "red")
+    ggplot2::geom_vline(xintercept = peaks - 0.5, size = 1, alpha = 0.4, color = "red") +
+    ggplot2::geom_vline(xintercept = peaks + 0.5, size = 1, alpha = 0.4, color = "red")
 
   p = cowplot::plot_grid(plotlist = list(p1, p2),
                          ncol = 1, align = "h", axis = "tblr")
 
   # Find top references of each peak year
   df = CR_data$references %>%
-    filter(Year_Cited %in% peaks) %>%
-    group_by(Year_Cited) %>%
-    distinct(ID_Cited, .keep_all = T) %>%
-    slice_max(order_by = Times_Cited, n = n_refs) %>%
-    select(ID_Cited, Reference_Cited, Year_Cited, Times_Cited, DOI_Cited)
+    dplyr::filter(Year_Cited %in% peaks) %>%
+    dplyr::group_by(Year_Cited) %>%
+    dplyr::distinct(ID_Cited, .keep_all = T) %>%
+    dplyr::slice_max(order_by = Times_Cited, n = n_refs) %>%
+    dplyr::select(ID_Cited, Reference_Cited, Year_Cited, Times_Cited, DOI_Cited)
 
   list(pubs = df, rpys = p1, rpys_stacked = p, peak_years = peaks)
 }
@@ -183,27 +189,28 @@ identify_milestones <- function (CR_data, n_milestones = 10, n_refs = 1, min_tim
 #' @param cited_year_range The range of years to plot. Default is NULL, which the function transforms to "from 1900 to the most recent year in the dataset".
 #'
 #' @return A ggplot.
+#' @importFrom magrittr %>%
 #' @export
 plot_rpys <- function (CR_data, cited_year_range = NULL) {
 
   df = CR_data$references %>%
-    count(Year_Cited) %>%
-    rename(`# References` = n) %>%
-    mutate(`Deviation from 5-yr Median` = detrend(`# References`)) %>%
-    pivot_longer(-Year_Cited)
+    dplyr::count(Year_Cited) %>%
+    dplyr::rename(`# References` = n) %>%
+    dplyr::mutate(`Deviation from 5-yr Median` = detrend(`# References`)) %>%
+    tidyr::pivot_longer(-Year_Cited)
 
   if (is.null(cited_year_range)) {
     max_py = min(max(df$Year_Cited), lubridate::year(lubridate::now()))
     cited_year_range = c(1900, max_py)
   }
 
-  p = ggplot(df) +
-    aes(x = Year_Cited, y = value, color = name) +
-    geom_line() +
-    xlim(cited_year_range) +
-    theme_minimal() +
-    theme(legend.position = "bottom") +
-    labs(x = "", y = "# Cited References", color = "")
+  p = ggplot2::ggplot(df) +
+    ggplot2::aes(x = Year_Cited, y = value, color = name) +
+    ggplot2::geom_line() +
+    ggplot2::xlim(cited_year_range) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "bottom") +
+    ggplot2::labs(x = "", y = "# Cited References", color = "")
 
   p
 }
@@ -219,13 +226,14 @@ plot_rpys <- function (CR_data, cited_year_range = NULL) {
 #' @param cited_year_range The range of years to plot. Default is NULL, which the function transforms to "from 1900 to the most recent year in the dataset".
 #'
 #' @return A ggplot.
+#' @importFrom magrittr %>%
 #' @export
 plot_multi_rpys <- function (CR_data, cited_year_range = NULL) {
 
   df = CR_data$references %>%
-    count(Year_Citing, Year_Cited) %>%
-    group_by(Year_Citing) %>%
-    mutate(
+    dplyr::count(Year_Citing, Year_Cited) %>%
+    dplyr::group_by(Year_Citing) %>%
+    dplyr::mutate(
       dt = detrend(n),
       r = rank(dt, ties.method = "min"),
       r = 100 + r - max(r)
@@ -236,15 +244,16 @@ plot_multi_rpys <- function (CR_data, cited_year_range = NULL) {
     cited_year_range = c(1950, max_py)
   }
 
-  p = ggplot(df) +
-    aes(x = Year_Cited, y = Year_Citing, fill = r) +
-    geom_tile() +
-    scale_fill_gradient2(low = "#f2f0df", mid = "#98db81", high = "#08004f", midpoint = 60) +
-    scale_x_continuous(limits = cited_year_range, breaks = scales::pretty_breaks(n = 8)) +
-    scale_y_reverse() +
-    theme_minimal() +
-    theme(legend.position = "bottom", panel.grid = element_blank()) +
-    labs(x = "Cited References", y = "Citing Documents", fill = "Ranked Absolute Deviation")
+  p = ggplot2::ggplot(df) +
+    ggplot2::aes(x = Year_Cited, y = Year_Citing, fill = r) +
+    ggplot2::geom_tile() +
+    ggplot2::scale_fill_gradient2(low = "#f2f0df", mid = "#98db81", high = "#08004f", midpoint = 60) +
+    ggplot2::scale_x_continuous(limits = cited_year_range, breaks = scales::pretty_breaks(n = 8)) +
+    ggplot2::scale_y_reverse() +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "bottom",
+                   panel.grid = ggplot2::element_blank()) +
+    ggplot2::labs(x = "Cited References", y = "Citing Documents", fill = "Ranked Absolute Deviation")
 
   p
 }
@@ -264,7 +273,7 @@ detrend <- function (x, window_size = 5) {
   for (i in 1:length(x)) {
     lower_i = max(1, i - half_size)
     upper_i = min(length(x), i + half_size)
-    m[i] = median(x[lower_i:upper_i])
+    m[i] = stats::median(x[lower_i:upper_i])
   }
   x - m
 }
